f5617bc0d8040736cc371cd8f7d6a67173938a5a
Katarina Milanov <kaca.milanov@gmail.com>
2022-03-18 12:23:18 +0100

    kreirani i povezani modeli

---
+++ aplikacijaLaravel/app/Models/Dress.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dress extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'model',
+        'color',
+        'releaseYear'
+    ];
+
+    public function designer(){
+        return $this->belongsTo(Designer::class);
+    }
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
+}
